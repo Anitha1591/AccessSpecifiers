8bd224f4406dc680dac95e35a12dbbb136c46ff2
Anitha1591 <ranithacse@gmail.com>
2023-02-03 19:08:01 +0530

    source code

---
+++ src/org/pgm/test/DataTypes.java
@@ -0,0 +1,30 @@
+package org.pgm.test;
+
+
+public class DataTypes {
+	
+		public static void main(String[] args) {
+			
+			byte weight=15;
+			System.out.println("Baby weight is" +weight);
+			short run = 100;
+			System.out.println("Total run is" +run);
+			int salary = 150000;
+			System.out.println("My salary is " +salary);
+			long attitude = 62458123l;
+			System.out.println("The attitude range is" + attitude);
+			float height = 5.5f;
+			System.out.println("My height is " +height);
+			double annualpay= 35000000;
+			System.out.println("My Annual Pay for a year is" +annualpay);
+			char initial = 'R';
+			System.out.println("My Initial is " + initial);
+			String place = "Tiruvarur";
+			System.out.println("My native place is" +place);
+			boolean food = true;
+			System.out.println("My favorite food is Briyani" +food);
+			
+					}	
+	}
+	
+
